d31b465c701eebe27be456f33a99ba6176f31c0b
Frederick Jaime <frejaime2@usnycpww2242.global.publicisgroupe.net>
2017-04-24 15:57:04 -0400

    adding index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express'),
+  request = require('request'),
+  bodyParser = require('body-parser'),
+  app = express();
+
+app.use(bodyParser.json());
+app.set('port', process.env.PORT || 5000);
+
+
+
+
+
+
+app.post('/webhook', (req, res) => {
+  let response;
+console.log(req.body.result.action);
+  switch (req.body.result.action) {
+    case 'willItRain':
+      response = willItRain(req.body.result.parameters);
+      break;
+    default:
+      response = getCurrentWeather(req.body.result.parameters);
+      break;
+  }
+
+  response.then((data) => {
+    res.set('Content-type', 'application/json');
+    res.send(data);
+  });
+});
+
+app.listen(app.get('port'), () => console.log(`app running on port ${app.get('port')}`));
+
+module.exports = app;
