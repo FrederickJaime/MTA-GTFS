4af0d729e53e4f3daf59896155ef2e285bc21ec4
Frederick Jaime <frejaime2@usnycpww2242.global.publicisgroupe.net>
2017-04-27 11:57:08 -0400

    starting to test things.

--- index.js
+++ index.js
@@ -11,22 +11,13 @@ app.set('port', process.env.PORT || 5000);
 
 
 
-app.post('/webhook', (req, res) => {
-  let response;
-console.log(req.body.result.action);
-  switch (req.body.result.action) {
-    case 'willItRain':
-      response = willItRain(req.body.result.parameters);
-      break;
-    default:
-      response = getCurrentWeather(req.body.result.parameters);
-      break;
-  }
 
-  response.then((data) => {
-    res.set('Content-type', 'application/json');
-    res.send(data);
-  });
+app.get('/webhook', (req, res) => {
+  let response;
+//console.log(req.body.result.action);
+
+  res.send("str");
+
 });
 
 app.listen(app.get('port'), () => console.log(`app running on port ${app.get('port')}`));
