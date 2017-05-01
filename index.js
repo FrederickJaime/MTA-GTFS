6e30435b038768b4fec98ca1776f05a78496dcc0
Frederick Jaime <frejaime2@usnycpww2242.global.publicisgroupe.net>
2017-05-01 16:49:07 -0400

    change method of pulling MTA data

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+
+"use strict";
 const express = require('express'),
   GtfsRealtimeBindings = require('mta-gtfs-realtime-bindings'),
   request = require('request'),
   rp = require('request-promise'),
   protobuf = require("protobufjs"),
+  ByteBuffer = require("bytebuffer"),
+  Long = require("long"),
+  Mta = require('mta-gtfs'),
   bodyParser = require('body-parser'),
   app = express(),
 
@@ -13,35 +18,62 @@ app.use(bodyParser.json());
 app.set('port', process.env.PORT || 5000);
 
 
-rp({
-    method: 'GET',
-    url: `http://datamine.mta.info/mta_esi.php?key=${mtaKey}${mtaFeedId}`,
-    encoding: null,
-}).then((buf) => {
-    const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(buf);
-    feed.entity.forEach((entity) => {
-        console.log(entity);
-        if (entity.trip_update) {
-            console.log(entity.trip_update);
-        }
+let mta = new Mta({
+  key: 'e853b54c8671a51a9f67a2d99f014264', // only needed for mta.schedule() method
+  feed_id: 1                  // optional, default = 1
+});
+
+
+
+
+
+
+
+/*
+mta.status('subway').then(function (result) {
+  console.log(result);
+});
+*/
+/*
+mta.schedule(635).then(function (result) {
+  console.log(result);
+});
+*/
+
+
+mta.stop().then(function (result) {
+  console.log(result);
+}).catch(function (err) {
+  console.log(err);
+});
+
+/*
+mta.stop(635).then(function (result) {
+  console.log(result);
+});
+mta.schedule(635).then(function (result) {
+  console.log(result);
+});
+*/
+
+
+    app.get('/webhook', (req, res) => {
+    let response;
+    console.log(req.body);
+
+    res.send(`<div>ddd</div>`);
+    
+
     });
-}).catch(e => {
-
-  console.log(e);
-
-});
 
 
 
 
-app.get('/webhook', (req, res) => {
-  let response;
- console.log(req.body);
 
-  res.send("str");
-  
 
-});
+
+
+
 
 app.listen(app.get('port'), () => console.log(`app running on port ${app.get('port')}`));
 
