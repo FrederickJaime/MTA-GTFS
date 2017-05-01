cb2c89561f8938d5f4302019c45cdbe2f285cae7
Frederick Jaime <frejaime2@usnycpww2242.global.publicisgroupe.net>
2017-05-01 09:54:54 -0400

    checking all is commited

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express'),
+  GtfsRealtimeBindings = require('mta-gtfs-realtime-bindings'),
   request = require('request'),
+  rp = require('request-promise'),
+  protobuf = require("protobufjs"),
   bodyParser = require('body-parser'),
   app = express();
 
@@ -7,16 +10,33 @@ app.use(bodyParser.json());
 app.set('port', process.env.PORT || 5000);
 
 
+rp({
+    method: 'GET',
+    url: 'http://datamine.mta.info/mta_esi.php?key=e853b54c8671a51a9f67a2d99f014264&feed_id=1',
+    encoding: null,
+}).then((buf) => {
+    const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(buf);
+    feed.entity.forEach((entity) => {
+        console.log(entity);
+        if (entity.trip_update) {
+            console.log(entity.trip_update);
+        }
+    });
+}).catch(e => {
 
+  console.log(e);
+
+});
 
 
 
 
 app.get('/webhook', (req, res) => {
   let response;
-//console.log(req.body.result.action);
+ console.log(req.body);
 
   res.send("str");
+  
 
 });
 
---
+++ node_modules/mta-gtfs-realtime-bindings/CHANGELOG.md
@@ -0,0 +1,8 @@
+[02/19/2017:KLANDELL] Forked https://github.com/google/gtfs-realtime-bindings  
+[02/19/2017:KLANDELL] Deleted all language bindings except nodejs  
+[02/19/2017:KLANDELL] Moved nodejs code out of dedicated directory to project root  
+[02/19/2017:KLANDELL] Added nyct-subway.proto file  
+[02/19/2017:KLANDELL] Moved nyct-subway.proto and gtfs-realtime.proto to proto directory    
+[02/19/2017:KLANDELL] Ran `npm run buildProto` to update gtfs-realtime.js     
+[02/19/2017:KLANDELL] Updated package.json and README    
+SEE COMMIT LOG FOR MORE DETAILED INFO
